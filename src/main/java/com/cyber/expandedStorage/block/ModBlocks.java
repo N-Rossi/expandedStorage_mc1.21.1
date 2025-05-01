fbc90a413379bccc942db240765a30b78397288c
Nicolas Rossi <nick413rossi@gmail.com>
2025-05-01 15:01:38 -0600

    Add gallium blocks and items (Can not break yet)

---
+++ src/main/java/com/cyber/expandedStorage/block/ModBlocks.java
@@ -0,0 +1,40 @@
+package com.cyber.expandedStorage.block;
+
+import com.cyber.expandedStorage.ExpandedStorage;
+import com.cyber.expandedStorage.item.ModItems;
+import net.minecraft.util.valueproviders.ConstantInt;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(ExpandedStorage.MOD_ID);
+
+    public static final DeferredBlock<Block> GALLIUM_BLOCK = registerBlock("gallium_block", () -> new Block(BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
+    public static final DeferredBlock<Block> GALLIUM_ORE = registerBlock("gallium_ore", () -> new DropExperienceBlock(
+            ConstantInt.of(0),
+            BlockBehaviour.Properties.of().mapColor(MapColor.STONE).requiresCorrectToolForDrops().strength(3.0F, 3.0F)));
+
+    private  static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
+        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
